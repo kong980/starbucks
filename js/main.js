8fd4411734248c6e99c4e58f4fcb8884e3851abe
Sarah <kong8249@naver.com>
2023-08-09 09:24:27 +0900

    08.08 스타벅스 두 번째 커밋 : 메뉴바, 메인 이미지 업로드

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ searchInputEl.addEventListener("blur", () => {
     searchEl.classList.remove('focused'); // 속성 삭제
     searchInputEl.setAttribute("placeholder","");
 });
+
+
+const badgeEl = document.querySelector('header .badges');
+
+/* lodash cdn 검색 : 너무 자주 실행되는 함수 제어 */
+// window.addEventListener("scroll", _.throttle(() => {
+//     // console.log(window.scrollY);
+//     if(window.scrollY > 500){
+//         badgeEl.style.display = "none";
+//     }else{
+//         badgeEl.style.display= "block";
+//     }
+// }, 300)); //(함수, 실행 시간 주기)
+
+
+/* gsap cdn 검색*/
+
+window.addEventListener("scroll", _.throttle(() => {
+    if(window.scrollY > 500){
+        // gsap.to(요소, 지속시간, 옵션)
+        gsap.to(badgeEl, .6, {opacity:0, display:"none"});
+    }else{
+        gsap.to(badgeEl, .6, {opacity:1, display:"block"});
+    }
+}, 300));
